e00f693aeb00b563877b37fb4cbd560b01647d54
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2021-11-03 18:04:15 -0400

    Validating user input

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 [1 - Project setup ](../../tree/ac200d795d45c4bfa6c0324462182cebb35196c4/)
 
-[2 - Empty ](../../tree//)
+[2 - Form submission with browser side JavaScript ](../../tree/63217ca4316de8815fc1df95692de52dcd035071/)
 
 [3 - Empty ](../../tree//)
 
--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,16 +1,25 @@
 function openPlayerConfig() {
-	playerConfigOverlayElement.style.display = 'block';
-	backdropElement.style.display = 'block';
+  playerConfigOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
 }
 
 function closePlayerConfig() {
-	playerConfigOverlayElement.style.display = 'none';
-	backdropElement.style.display = 'none';
+  playerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
-	event.preventDefault();
-	const formData = new FormData(event.target);
-	const enteredPlayerName = formData.get('playername');
-	console.log(enteredPlayerName);
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername').trim(); // '      ' => ''
+
+  if (!enteredPlayername) { // enteredPlayername === ''
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
+
+
 }
