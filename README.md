ac200d795d45c4bfa6c0324462182cebb35196c4
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2021-11-03 16:41:23 -0400

    Project Setup

---
+++ README.md
@@ -0,0 +1,20 @@
+### Tic-Tac-Toe game
+
+#### HTML, CSS, JavaScript 
+
+
+[1 - Project setup ](../../tree//)
+
+[2 - Empty ](../../tree//)
+
+[3 - Empty ](../../tree//)
+
+[4 - Empty ](../../tree//)
+
+[5 -  Empty ](../../tree//)
+
+[8 -  Empty ](../../tree//)
+
+[9 -  Empty ](../../tree//)
+
+[10 - Empty ](../../tree//)
---
+++ src/scripts/app.js
@@ -0,0 +1,12 @@
+const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
+
+const editPlayer1BtnElement = document.getElementById('edit_player_1_btn');
+const editPlayer2BtnElement = document.getElementById('edit_player_2_btn');
+const cancelConfigBtnElement = document.getElementById('cancel_config_button');
+
+editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
+editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
+
+cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
